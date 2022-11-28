89780e6daf977ca4d7410c307b2d392491a5368f
Jan Gregor Emge-Triebel <jan.emgetriebel@shopify.com>
2022-11-28 10:15:44 +0100

    Clean up and linux support (untested)

--- tests/run.sh
+++ tests/run.sh
@@ -1,28 +1,63 @@
-content=$(vale --output=JSON good)
-if [ "$content" == "{}" ] ;then
+#!/usr/bin/env bash
+
+#
+# Determine the platform to find correct jq binary to use
+#
+if [ "$(uname)" == "Darwin" ]; then
+    jqBinary="./bin/jq-osx-amd64"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    jqBinary="./bin/jq-linux64"
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    echo "Win32 is not supported"
+    exit 1
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    echo "Win64 is not supported"
+    exit 1
+fi
+
+#
+# Run vale for `good` folder and store output
+#
+vale --output=JSON good > './tmp/good.json'
+
+#
+# Use https://stedolan.github.io/jq/manual/#length to count encountered errors
+# We expect 0 because all examples in the good folder should pass
+#
+expectedErrorCount=0
+hits=$(cat ./tmp/good.json | ${jqBinary} '. | length')
+if [ $hits == $expectedErrorCount ] ;then
     echo "All good examples passed!"
     passedGood=0
 else
-    echo "Unexpected results from good examples:"
-    echo $content
+    echo "Error when running 'good' test scenarios:"
+    echo "Unexpected result count. Should be ${expectedErrorCount} but found ${hits}."
+    cat ./tmp/good.json
     passedGood=1
 fi
 
-## Use https://stedolan.github.io/jq/manual/#TypesandValues to validate ?
-
+#
+# Run vale for `bad` folder and store output
+#
 vale --output=JSON bad > './tmp/bad.json'
-#echo ./bin/jq-osx-amd64 -f './tmp/bad.json' | length
 
-hits=$(cat ./tmp/bad.json | ./bin/jq-osx-amd64 '. | length')
-
-if [ $hits == 1 ] ;then
+#
+# Use https://stedolan.github.io/jq/manual/#length to count encountered errors
+# We expect 1 because all bad examples are intentionally added here
+#
+expectedErrorCount=1
+hits=$(cat ./tmp/bad.json | ${jqBinary} '. | length')
+if [ $hits == $expectedErrorCount ] ;then
     echo "Bad result count is as expected"
     passedBad=0
 else
-    echo "Unexpected bad result count:"
+    echo "Error when running 'bad' test scenarios:"
+    echo "Unexpected result count. Should be ${expectedErrorCount} but found ${hits}."
     cat ./tmp/bad.json
     passedBad=1
 fi
 
+#
 # Will let this exit with a non zero exit code if either case above hit errors
+#
 exit $((passedBad+passedBad))
