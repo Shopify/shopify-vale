7233389f6fe23e44cba00e7dee9febd2690b3899
Jan Gregor Emge-Triebel <jan.emgetriebel@shopify.com>
2022-11-30 12:07:14 +0100

    Test script adapted for linux/ci

--- tests/run.sh
+++ tests/run.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
+set -euxo pipefail
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 #
 # Determine the platform to find correct jq binary to use
 #
 if [ "$(uname)" == "Darwin" ]; then
-    jqBinary="./bin/jq-osx-amd64"
+    jqBinary="${SCRIPTPATH}/bin/jq-osx-amd64"
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    jqBinary="./bin/jq-linux64"
+    jqBinary="${SCRIPTPATH}/bin/jq-linux64"
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     echo "Win32 is not supported"
     exit 1
@@ -18,46 +20,48 @@ fi
 #
 # Run vale for `good` folder and store output
 #
-vale --output=JSON good > './tmp/good.json'
+vale --output=JSON "${SCRIPTPATH}/good" > "${SCRIPTPATH}/tmp/good.json"
 
 #
 # Use https://stedolan.github.io/jq/manual/#length to count encountered errors
 # We expect 0 because all examples in the good folder should pass
 #
 expectedErrorCount=0
-hits=$(cat ./tmp/good.json | ${jqBinary} '. | length')
-if [ $hits == $expectedErrorCount ] ;then
+hits=$(cat ${SCRIPTPATH}/tmp/good.json | ${jqBinary} '. | length')
+if [ "$hits" == "$expectedErrorCount" ] ;then
     echo "All good examples passed!"
     passedGood=0
 else
     echo "Error when running 'good' test scenarios:"
     echo "Unexpected result count. Should be ${expectedErrorCount} but found ${hits}."
-    cat ./tmp/good.json
+    cat ${SCRIPTPATH}/tmp/good.json
     passedGood=1
 fi
 
 #
 # Run vale for `bad` folder and store output
 #
-vale --output=JSON bad > './tmp/bad.json'
+vale --output=JSON "${SCRIPTPATH}/bad" > "${SCRIPTPATH}/tmp/bad.json"
 
 #
 # Use https://stedolan.github.io/jq/manual/#length to count encountered errors
-# We expect 1 because all bad examples are intentionally added here
+# We expect them to be equal the number of files in the directory
+# We need to add 0 to the count result in order to 'typecast' it into an integer
 #
-expectedErrorCount=$(find bad -type f -name '*.md' | wc -l)
-hits=$(cat ./tmp/bad.json | ${jqBinary} '. | length')
-if [ $hits == $expectedErrorCount ] ;then
+expectedErrorCount=$(find ${SCRIPTPATH}/bad -type f -name '*.md' | wc -l)
+expectedErrorCount="$(($expectedErrorCount + 0))"
+hits=$(cat ${SCRIPTPATH}/tmp/bad.json | ${jqBinary} '. | length')
+if [ "$hits" == "$expectedErrorCount" ] ;then
     echo "Bad result count is as expected"
     passedBad=0
 else
     echo "Error when running 'bad' test scenarios:"
     echo "Unexpected result count. Should be ${expectedErrorCount} but found ${hits}."
-    cat ./tmp/bad.json
+    cat ${SCRIPTPATH}/tmp/bad.json
     passedBad=1
 fi
 
 #
 # Will let this exit with a non zero exit code if either case above hit errors
 #
-exit $((passedBad+passedBad))
+exit $((passedBad+passedGood))
